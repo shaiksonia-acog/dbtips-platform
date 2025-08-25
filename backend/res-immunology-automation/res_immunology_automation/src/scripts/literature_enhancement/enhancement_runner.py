c09fc08ade6403c9911e5c6844df3194d67a053c
amani <amani@aganitha.ai>
2025-08-25 07:41:18 +0000

    fixed logging format

--- backend/res-immunology-automation/res_immunology_automation/src/scripts/literature_enhancement/enhancement_runner.py
+++ backend/res-immunology-automation/res_immunology_automation/src/scripts/literature_enhancement/enhancement_runner.py
@@ -1,14 +1,15 @@
 import sys
 sys.path.append('/app/res-immunology-automation/res_immunology_automation/src/scripts/')
-from analyzer.literature_analyzer import run_analyzers
-from data_segregation.literature_segregation import run_literature_segregation
-from literature_extractor.literature_extractor import extract_literature
+from literature_enhancement.analyzer.literature_analyzer import run_analyzers
+from literature_enhancement.data_segregation.literature_segregation import run_literature_segregation
+from literature_enhancement.literature_extractor.literature_extractor import extract_literature
 import logging
 import asyncio
 
 from literature_enhancement.config import LOGGING_LEVEL
 logging.basicConfig(level=LOGGING_LEVEL)
-logger = logging.getLogger(__name__)
+module_name = os.path.splitext(os.path.basename(__file__))[0].upper()
+logger = logging.getLogger(module_name)
 
 # async def run_enhancement_pipeline(disease: str = "no-disease", target: str = "no-target"):
 #     logger.info(f"Starting enhancement pipeline for disease: {disease}, target: {target}")
@@ -44,7 +45,7 @@ async def run_enhancement_pipeline(disease: str = "no-disease", target: str = "n
         raise e
 
 if __name__ == "__main__":
-    disease = "phenylketonuria"
+    disease = "asthma"
     try:
         asyncio.run(run_enhancement_pipeline(disease))
     except Exception as e:
