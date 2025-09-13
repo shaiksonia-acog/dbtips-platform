6c458864ad2525d9c75a52d2086c938242d11abd
deepanshu-acog <deepanshu@aganitha.ai>
2025-09-14 00:49:41 +0530

    fix: External links

--- frontend/src/pages/evidence/carousel.tsx
+++ frontend/src/pages/evidence/carousel.tsx
@@ -7,12 +7,9 @@ import {
   Col,
   ConfigProvider,
   Empty,
-  Tooltip,
   Modal,
 } from "antd";
-import { InfoCircleOutlined } from "@ant-design/icons";
 import { ExternalLinkIcon } from "lucide-react";
-
 
 const isPresent = (val: any) => val !== null && val !== undefined && val !== "";
 
@@ -62,9 +59,7 @@ const CarouselComponent = ({ networkBiologyData }) => {
               <Row gutter={[16, 16]}>
                 {chunk.map((image: any, idx: number) => (
                   <Col key={idx} span={8}>
-                    <div
-                        onClick={(e) => handleInfoClick(image, e)}
-                    >
+                    <div onClick={(e) => handleInfoClick(image, e)}>
                       <Card
                         hoverable
                         className="custom-card relative"
@@ -76,22 +71,20 @@ const CarouselComponent = ({ networkBiologyData }) => {
                               className="w-full h-auto"
                             />
                             {/* Info Icon */}
-                            <Tooltip title="More info about this figure">
                             <a
-                      href={
-                        image.pmcid
-                          ? `https://pmc.ncbi.nlm.nih.gov/articles/${image.pmcid}`
-                          : `https://pubmed.ncbi.nlm.nih.gov/${image.pmid}`
-                      }
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    ><ExternalLinkIcon className="absolute top-2 left-2 text-blue-500 hover:text-blue-700 h-6 w-5" /></a>
-                              <InfoCircleOutlined
-                                className="absolute top-2 right-2 text-blue-500 hover:text-blue-700"
-                                style={{ fontSize: 20 }}
-                             
-                              />
-                            </Tooltip>
+                              href={
+                                image.pmcid
+                                  ? `https://pmc.ncbi.nlm.nih.gov/articles/${image.pmcid}`
+                                  : `https://pubmed.ncbi.nlm.nih.gov/${image.pmid}`
+                              }
+                              className="absolute top-2 right-2"
+                              target="_blank"
+                              onClick={(e) => e.stopPropagation()}
+                              rel="noopener noreferrer"
+                            >
+                              <ExternalLinkIcon className=" text-blue-500 hover:text-blue-700 h-6 w-5"  />
+                            </a>
+                           
                           </div>
                         }
                       >
@@ -103,17 +96,15 @@ const CarouselComponent = ({ networkBiologyData }) => {
                                 whiteSpace: "normal",
                                 maxHeight: "100px",
                                 // lineClamp: 3,
-                                
+
                                 textOverflow: "ellipsis",
                                 display: "-webkit-box",
-                                WebkitLineClamp: 3, 
+                                WebkitLineClamp: 3,
                                 WebkitBoxOrient: "vertical",
-                        
-              
                               }}
                             >
                               {/* <Tooltip title={image.figtitle}> */}
-                                <span>{image.figtitle}</span>
+                              <span>{image.figtitle}</span>
                               {/* </Tooltip> */}
                             </div>
                           }
@@ -133,34 +124,31 @@ const CarouselComponent = ({ networkBiologyData }) => {
         open={isModalOpen}
         onCancel={() => setIsModalOpen(false)}
         footer={null}
-          title={
-        <div className="flex justify-end mr-6 -mt-1 items-center ">
-        
-          <a
-            href={
-              selectedImage?.pmcid
-                ? `https://pmc.ncbi.nlm.nih.gov/articles/${selectedImage.pmcid}`
-                : selectedImage?.pmid
-                ? `https://pubmed.ncbi.nlm.nih.gov/${selectedImage.pmid}`
-                : "#"
-            }
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={(e) => e.stopPropagation()}
-          >
-          
+        title={
+          <div className="flex justify-end mr-6 -mt-1 items-center ">
+            <a
+              href={
+                selectedImage?.pmcid
+                  ? `https://pmc.ncbi.nlm.nih.gov/articles/${selectedImage.pmcid}`
+                  : selectedImage?.pmid
+                  ? `https://pubmed.ncbi.nlm.nih.gov/${selectedImage.pmid}`
+                  : "#"
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={(e) => e.stopPropagation()}
+            >
               <ExternalLinkIcon className="text-blue-500 hover:text-blue-700 h-6 w-5" />
-          </a>
-        </div>
-      } 
-          
+            </a>
+          </div>
+        }
         centered
         width={1400}
       >
         {selectedImage && (
           <Row gutter={24} align="top">
             {/* Left: Image */}
-            <Col span={12} >
+            <Col span={12}>
               <img
                 src={selectedImage.image_url}
                 alt={selectedImage.figtitle}
@@ -170,27 +158,20 @@ const CarouselComponent = ({ networkBiologyData }) => {
 
             {/* Right: Content */}
             <Col span={10}>
-
               <div className="card-back-content">
-                {selectedImage.figtitle && isPresent(selectedImage.figtitle) && (
-                  <div className="pb-2 mb-2 border-b-2">
-                    <h3 className="text-lg font-semibold mb-1">
-                      Caption:
-                    </h3>
-                    <p
-                      className=" leading-tight text-base"
-                    >
-                      {selectedImage.figtitle}
-                    </p>
-                  </div>
-                )}
+                {selectedImage.figtitle &&
+                  isPresent(selectedImage.figtitle) && (
+                    <div className="pb-2 mb-2 border-b-2">
+                      <h3 className="text-lg font-semibold mb-1">Caption:</h3>
+                      <p className=" leading-tight text-base">
+                        {selectedImage.figtitle}
+                      </p>
+                    </div>
+                  )}
                 {selectedImage.drugs && isPresent(selectedImage.drugs) && (
                   <div className="pb-2 mb-2 border-b-2">
-<h3 className="text-lg font-semibold mb-1">                      Drugs:
-                    </h3>
-                    <p
-                      className=" leading-tight text-base"
-                    >
+                    <h3 className="text-lg font-semibold mb-1"> Drugs:</h3>
+                    <p className=" leading-tight text-base">
                       {selectedImage.drugs}
                     </p>
                   </div>
@@ -198,27 +179,22 @@ const CarouselComponent = ({ networkBiologyData }) => {
 
                 {selectedImage.process && isPresent(selectedImage.process) && (
                   <div className="pb-2 mb-2 border-b-2">
-                    <h3 className="text-lg font-semibold mb-1">                      Process:
-                    </h3>
-                    <p
-                      className=" leading-tight text-base"
-                    >
+                    <h3 className="text-lg font-semibold mb-1"> Process:</h3>
+                    <p className=" leading-tight text-base">
                       {selectedImage.process}
                     </p>
                   </div>
                 )}
 
-                {selectedImage.insights && isPresent(selectedImage.insights) && (
-                  <div className="pb-2 mb-2 border-b-2">
-                    <h3 className="text-lg font-semibold mb-1">                      Insights:
-                    </h3>
-                    <p
-                      className=" leading-tight text-base"
-                    >
-                      {selectedImage.insights}
-                    </p>
-                  </div>
-                )}
+                {selectedImage.insights &&
+                  isPresent(selectedImage.insights) && (
+                    <div className="pb-2 mb-2 border-b-2">
+                      <h3 className="text-lg font-semibold mb-1"> Insights:</h3>
+                      <p className=" leading-tight text-base">
+                        {selectedImage.insights}
+                      </p>
+                    </div>
+                  )}
               </div>
             </Col>
           </Row>
