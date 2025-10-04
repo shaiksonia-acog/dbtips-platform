a3b600f8dea9e2b43f222b39cba67ef1f64bddc7
deepanshu-acog <deepanshu@aganitha.ai>
2025-10-04 15:17:59 +0530

    fix(targetApprovedDrug): correct filtering of approved drugs based on selected disease

--- frontend/src/pages/market-intelligence/targetApprovedDrug.tsx
+++ frontend/src/pages/market-intelligence/targetApprovedDrug.tsx
@@ -161,7 +161,7 @@ const ApprovedDrug = ({
     const data=approvedDrugData?.target_pipeline;
     const filtered =selectedDisease.length===0
       ? approvedDrugData?.target_pipeline?.target_pipeline?.filter((data) => data.ApprovalStatus === "Approved")
-      : approvedDrugData?.target_pipeline?.filter(
+      : approvedDrugData?.target_pipeline?.target_pipeline.filter(
           (data) =>
             selectedDisease.some(
               (disease) => disease.toLowerCase() === data.Disease.toLowerCase()
