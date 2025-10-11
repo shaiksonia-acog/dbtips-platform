f8dac074a625085ea915521ceeb3a0b203d4312e
deepanshu-acog <deepanshu@aganitha.ai>
2025-10-11 21:28:33 +0530

    Add sorting to p-value column in AssociatePlot

--- frontend-locked/src/pages/data/associatePlot.tsx
+++ frontend-locked/src/pages/data/associatePlot.tsx
@@ -204,6 +204,7 @@ const AssociatePlot = ({ indications,diseaseAreaFilter }) => {
     {
       headerName: "p-value",
       field: "pvalue",
+      sort:"asc"
     },
     {
       headerName: "RAF",
