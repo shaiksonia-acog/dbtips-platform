c40ced75fc45e10fa5578a5e451da3a2948ed789
deepanshu-acog <deepanshu@aganitha.ai>
2025-10-16 14:58:45 +0530

    Add description paragraph to Gene Essentiality Map for clarity

--- frontend-locked/src/pages/target-assessment/geneMap.tsx
+++ frontend-locked/src/pages/target-assessment/geneMap.tsx
@@ -407,6 +407,9 @@ const handleLegendClick = (datasetIndex: number) => {
   return (
     <div className="py-10" id="geneEssentialityMap">
       <h1 className="text-3xl font-semibold mb-3">Gene essentiality map</h1>
+      <p>
+       Shows how essential  {target} is across multiple cell lines, helping identify genes that are critical for cell survival or specific to certain cancer types.
+      </p>
       <div className="flex gap-2">
         <SearchBar
           searchTerm={searchTerm}
