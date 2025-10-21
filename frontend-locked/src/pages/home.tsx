4486105ade730a020752ed43affb85022fbd56e3
deepanshu-acog <deepanshu@aganitha.ai>
2025-10-21 18:59:25 +0530

    change target names

--- frontend-locked/src/pages/home.tsx
+++ frontend-locked/src/pages/home.tsx
@@ -15,7 +15,7 @@ type FieldType = {
   diseaseArea?: string[];
 };
 
-const targetOptions = ["ACTRIIA","ACTRIIB","APLNR","CAMK2D", "CRHR2","DNM1L", "ENHO","GPR75","INHBE","GIPR", "GUCY1A1","GUCY1B1", "PDE3B","SAV1","SVEP1","USP30"]
+const targetOptions = ["ACVR2A","ACVR2B","APLNR","CAMK2D", "CRHR2","DNM1L", "ENHO","GPR75","INHBE","GIPR", "GUCY1A1","GUCY1B1", "PDE3B","SAV1","SVEP1","USP30"]
 const diseaseAreaOptions = [ "Cardiovascular diseases", "Urologic diseases"]
 const indicationOptions = ["Obesity"]
 const { Option } = Select;
@@ -26,7 +26,7 @@ const diseaseToTargetMap: Record<string, string[]> = {
 };
 
 const indicationToTargetMap: Record<string, string[]> = {
-  "Obesity": ["ACTRIIA","ACTRIIB","APLNR","CRHR2","GIPR","GPR75","INHBE","PDE3B"]
+  "Obesity": ["ACVR2A","ACVR2B","APLNR","CRHR2","GIPR","GPR75","INHBE","PDE3B"]
 };
 const HomeLocked = ({ setAppState }) => {
 
